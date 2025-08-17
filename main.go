3bdb540b574201a651708ac5df84f5a4ef5e7b3f
Victoria Cheng <victoria.cheng15@gmail.com>
2025-08-17 13:16:21 -0400

    add recursive yaml parser to get config keys and values

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+
+    "gopkg.in/yaml.v3"
+)
+
+
+func printYAML(key string, value interface{}, indent int) {
+    pad := ""
+    for i := 0; i < indent; i++ {
+        pad += "  "
+    }
+    switch v := value.(type) {
+    case map[string]interface{}:
+        fmt.Printf("%s%v:\n", pad, key)
+        for k, val := range v {
+            printYAML(k, val, indent+1)
+        }
+    case []interface{}:
+        fmt.Printf("%s%v:\n", pad, key)
+        for i, item := range v {
+            printYAML(fmt.Sprintf("[%d]", i), item, indent+1)
+        }
+    default:
+        fmt.Printf("%s%v: %v\n", pad, key, v)
+    }
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+    configFile := "src/config/config.yml"
+    data, err := ioutil.ReadFile(configFile)
+    if err != nil {
+        log.Fatalf("Error reading config.yml: %v", err)
+    }
+
+    var config map[string]interface{}
+    err = yaml.Unmarshal(data, &config)
+    if err != nil {
+        log.Fatalf("Error parsing config.yml: %v", err)
+    }
+
+    for key, value := range config {
+        printYAML(key, value, 0)
+    }
 }
